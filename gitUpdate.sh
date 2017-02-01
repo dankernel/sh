41ca6e38eef4515abe7866ca6ca12d0bd18d4c75
dankernel <google@dankook.ac.kr>
2017-02-01 19:33:07 +0000

    20170201-193305-날짜 추가

--- gitUpdate.sh
+++ gitUpdate.sh
@@ -1,10 +1,17 @@
-echo -e "\033[31m"=== Git 업데이트 ==="\033[0m"
+#!/bin/bash
 
+# Date
+Date=`date +%Y%m%d'-'%H%M%S`
+
+# Print
+echo -e "\033[31m"=== Git 업데이트 ==="\033[0m"
 echo -e -n "\033[32m"=== 커밋 메세지 : "\033[0m"
 read message
 
+# Run
 git add .
-git commit -m "$message"
+git commit -m $Date'-'"$message"
 git push
 
+# End
 echo -e "\033[32m"=== [완료] ==="\033[0m"
